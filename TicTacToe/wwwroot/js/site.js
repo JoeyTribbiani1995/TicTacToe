ad1cb270c71289c0b67db3fe3efe4d84d7342f1e
JoeyTribbiani <vophanhongdung@gmail.com>
2019-07-04 16:17:15 +0700

    working with bundler and minifier generating file min

---
+++ TicTacToe/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+var interval;
+function EmailConfirmation(email) {
+    interval = setInterval(() => {
+        CheckEmailConfirmationStatus(email);
+    }, 5000);
+}
+
+function CheckEmailConfirmationStatus(email) {
+    $.get("/CheckEmailConfirmationStatus?email=" + email,
+        function (data) {
+            if (data === "OK") {
+                if (interval !== null) {
+                    clearInterval(interval);
+                }
+                window.location.href = "/GameInvitation/Index?email=" + email;
+            }
+        });
+}
